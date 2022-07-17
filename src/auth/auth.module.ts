f047edcc8a083d8450538ecc7b683cb49b1e904f
1ON3 <opppyshow@gmail.com>
2022-07-17 17:44:51 +0300

    Added JwtAuthGuard to get all users endpoint. Now only authorized users can get users list

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import {UsersModule} from "../users/users.module";
@@ -8,13 +8,17 @@ import {JwtModule} from "@nestjs/jwt";
   providers: [AuthService],
   controllers: [AuthController],
   imports: [
-      UsersModule,
+      forwardRef(() => UsersModule) ,
       JwtModule.register({
         secret: process.env.SECRET_KEY || 'SeCrEtKeY',
         signOptions: {
           expiresIn: '24h',
         }
       })
-  ]
+  ],
+    exports:[
+        AuthService,
+        JwtModule
+    ]
 })
 export class AuthModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import {SequelizeModule} from "@nestjs/sequelize";
@@ -7,12 +7,14 @@ import {UserRoles} from "../user-roles/user-roles.model";
 import {UserUserRoles} from "../user-roles/user-user-role.model";
 import {UserRolesModule} from "../user-roles/user-roles.module";
 import {CompanyModule} from "../company/company.module";
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
   imports: [
       SequelizeModule.forFeature([User, UserRoles, UserUserRoles]),
       UserRolesModule,
       CompanyModule,
+      forwardRef(() => AuthModule),
   ],
   providers: [UsersService],
   controllers: [UsersController],
